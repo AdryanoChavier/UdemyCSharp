b7eecf90987446827955610a7a9409c9f351f70e
adryano.chavier <adryano.chavier@senac.br>
2024-05-22 13:46:45 -0300

    Dia 22/05

---
+++ EntradadeDados_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //    int n1 = int.Parse(Console.ReadLine());
+        //    char ch = char.Parse(Console.ReadLine());
+        //    double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+           string[] i = Console.ReadLine().Split(' ');
+           string nome = i[0];
+           char sexo = char.Parse(i[1]);
+           int idade = int.Parse(i[2]);
+           double altura = double.Parse(i[3], CultureInfo.InvariantCulture);
+           
+        //    Console.WriteLine(n1);
+        //    Console.WriteLine(ch);
+        //    Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine(nome);
+        Console.WriteLine(sexo);
+        Console.WriteLine(idade);
+        Console.WriteLine(altura.ToString(CultureInfo.InvariantCulture));
+        
+        
+
+        }
+    }
+}
---
+++ Exercício_EntradaDados/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {   Console.Write("Entre com seu nome completo: ");
+            string nome = Console.ReadLine();
+            Console.Write("Quantos quartos tem na sua casa: ");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o preço do produto: ");
+            double preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);    
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha)");
+            string[] i = Console.ReadLine().Split(' ');
+            string sobrenome = i[0];
+            int idade = int.Parse(i[1]);
+            double altura = double.Parse(i[2],CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine(sobrenome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2",CultureInfo.InvariantCulture));
+        
+
+
+
+        }
+    }
+}
